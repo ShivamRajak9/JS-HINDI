aa79adfdf686313d666f691f949d8f2ee6c6d59c
ShivamRajak9 <143510143+ShivamRajak9@users.noreply.github.com>
2024-03-07 06:58:57 +0000

    Math

---
+++ 01_Basics/02_DataType.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code newer vesrion
+
+//alert("hello") //we are using nodejs, not browser
+
+console.log(3+3);
+
+let name = "shivam"
+let age =23
+let isLoggedIn = false
+let state;
+
+//number =>2 to power 53
+//bigInt 
+//String=>""
+//boolean=>true/false
+//null=>standalone value
+//undifined => 
+//symbol =>unique
+
+
+//object
+
+console.log(typeof undefined); //undifiend
+console.log(typeof null); //object

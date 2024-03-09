dcbc529cf14eff740b9e238284ffc48164512b82
ShivamRajak9 <143510143+ShivamRajak9@users.noreply.github.com>
2024-03-09 18:36:06 +0000

    FunctionWithObject

---
+++ 02_Basics/02_FunctionWithObjects.js
@@ -0,0 +1,37 @@
+//... restoperator
+function calculateCartPrice (val1, val2,...num1) 
+{
+    return num1
+}
+
+//console.log(calculateCartPrice(200,400,500,500,600));
+
+const user =
+{
+    username: "hitesh",
+    price:99
+}
+
+function handleObject(anyobject)
+{
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+//handleObject(user);
+
+handleObject(
+    {
+        username:"sam",
+        price:399
+    }
+)
+
+
+const myNewArray  = [200, 300,400,600]
+
+function returnSecondValue(getArray)
+{
+    return getArray[2]
+}
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,300,100,400]));

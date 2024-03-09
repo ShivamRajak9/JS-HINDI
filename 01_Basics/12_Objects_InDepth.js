4af8efb957e72e59ae0ffa93e2d606c420415e14
ShivamRajak9 <143510143+ShivamRajak9@users.noreply.github.com>
2024-03-09 18:35:31 +0000

    Object2

---
+++ 01_Basics/12_Objects_InDepth.js
@@ -0,0 +1,25 @@
+const course =
+{
+    coursename:"Full stack Devloper",
+    price:"1000",
+    courseInstructor:"Shivam"
+}
+//course.courseInstructor
+
+const{courseInstructor: instructor} = course
+
+console.log(instructor);
+
+//jason api
+
+// {
+//     "name": "hitesh",
+//     "coursename":"js in hindi",
+//     "price":"free"
+// }
+
+[
+    {},
+    {},
+    {}
+]

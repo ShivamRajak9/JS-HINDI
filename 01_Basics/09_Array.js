8a70afb66c9298a73d16dd16c1c9673ef9de72c7
ShivamRajak9 <143510143+ShivamRajak9@users.noreply.github.com>
2024-03-09 16:42:39 +0000

    Array

---
+++ 01_Basics/09_Array.js
@@ -0,0 +1,33 @@
+// //Array
+
+const myArr = [0,1,2,3,4,5,6]
+// const myHeros = ["ironman","batman","thor"]
+// const myArr2 = new Array(1,2,3,4)
+// console.log(myArr[1]);
+
+// //Array Method
+
+// myArr.push(7)
+// myArr.pop(7)
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr);
+
+// console.log(myArr.includes(8));
+// console.log(myArr.indexOf(10));
+
+// const newArr = myArr.join()
+// console.log(typeof newArr);
+
+// slice splice 
+
+console.log("A ", myArr);
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log("b ",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("c ",myArr);
+console.log(myn2);
